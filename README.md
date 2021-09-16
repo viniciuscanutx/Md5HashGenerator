cf81efb0940bd3e7520f33550074bb554b88dac9
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-09-16 14:18:44 -0300

    Adicionando coisas ao README e traduzindo o código!

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Aqui vamos fazer um conversor de texto para MD5.
 (Atualizações Abaixo)
 #VamosLá!
 
-1- Começando o projeto
-2- 
+1- Começando o projeto \\ 14:00 16/09/2021
+2- Estrutura do Projeto Feita \\ 14:17 16/09/2021
 
--- md5hash.py
+++ md5hash.py
@@ -5,6 +5,6 @@ textoinit = input("O que deseja converter?")
 result = hashlib.md5(textoinit.encode())
   
 # printing the equivalent hexadecimal value.
-print("The hexadecimal equivalent of hash is : ", end ="")
+print("O seu texto em md5 é: ", end ="")
 print(result.hexdigest())
 
