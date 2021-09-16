6d4abd0a5a0a947371968dc73a84548e8723273b
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-09-16 16:11:45 -0300

    Changes Readme

--- md5hash.py
+++ md5hash.py
@@ -1,8 +1,10 @@
+
 #Importação da Hashlib, a biblioteca que vai nos permitir transformar o texto em md5.
 import hashlib
 
+#Configuração para converter o texto.
 textoinit = input("Digite o que deseja converter: ") #O input que permite que você escreva o que deseja converter.
-result = hashlib.md5(textoinit.encode())
+result = hashlib.md5(textoinit.encode()) #Resultado da conversão do encode.
     
 #Parte Final.
 print("O seu texto em md5 é: ", end ="")#O Que mostra seu md5 na tela.
