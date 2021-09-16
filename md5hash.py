0a4d995a0fa37249c752c21cb13aae77d766230d
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-09-16 14:16:26 -0300

    concertando o codigo!

--- md5hash.py
+++ md5hash.py
@@ -1,8 +1,10 @@
 import hashlib
+  
+textoinit = input("O que deseja converter?")
+  
+result = hashlib.md5(textoinit.encode())
+  
+# printing the equivalent hexadecimal value.
+print("The hexadecimal equivalent of hash is : ", end ="")
+print(result.hexdigest())
 
-textonormal = input("Qual o texto que deseja converter?")
-
-def md5hasher(data):
-    md5hash = hashlib.md5()
-    md5hash.update(data.encode("utf8"))
-    print(md5hash.hexdigest())
