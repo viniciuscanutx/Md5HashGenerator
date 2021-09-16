1fe0af7a93d2475a9b99bdc46489cf3c17795575
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-09-16 14:22:54 -0300

    Projeto quase finalizado, vamos testar

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Aqui vamos fazer um conversor de texto para MD5.
 
 1- Começando o projeto \\ 14:00 16/09/2021
 2- Estrutura do Projeto Feita \\ 14:17 16/09/2021
+3- Terminamos o Projeto e vamos começar os testes. \\ 14:21 16/09/2021
+4- E se fizessemos uma interface para esse projeto? \\ 
 
--- md5hash.py
+++ md5hash.py
@@ -1,10 +1,11 @@
+#Importação da Hashlib, a biblioteca que vai nos permitir transformar o texto em md5.
 import hashlib
   
-textoinit = input("O que deseja converter?")
-  
-result = hashlib.md5(textoinit.encode())
-  
-# printing the equivalent hexadecimal value.
-print("O seu texto em md5 é: ", end ="")
-print(result.hexdigest())
+#Váriaveis que vão fazer nosso aplicativo funcionar.
+textoinit = input("O que deseja converter?") #O input que permite que você escreva o que deseja converter.
+result = hashlib.md5(textoinit.encode()) #Essa parte faz a conversão do texto pra md5.
+
+#Resultado e o que vai aparecer na tela do usuário.
+print("O seu texto em md5 é: ", end ="") #Esse Print é de orientação ao usuário.
+print(result.hexdigest()) #Print que mostra o resultado na tela do usuário.
 
